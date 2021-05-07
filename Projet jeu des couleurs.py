6ceb4a0f30b5321178a4694b425bb143ddd3eced
Amélie <amelie.negrier@ens.uvsq.fr>
2021-05-07 12:43:05 +0200

    07/05

--- Projet jeu des couleurs.py
+++ Projet jeu des couleurs.py
@@ -18,11 +18,12 @@ canvas=Canvas(racine, width=300, height= 900, bg="azure2")
 canvas2=Canvas(racine, width=300, height= 900, bg="azure2")
 
 def réinitialiser():
+    """Fonction qui remet le score à 0 et qui remet le compte à rebours à 30 secondes"""
     pass
 
 #ajout des fonctions
 def incremente():
-    "Incrémente le compteur à chaque seconde"
+    """Fonction qui génère un compte à rebours de 30 secondes"""
     global compteur
     compteur -=1
     compteur_lbl['text'] = str(compteur)
@@ -34,8 +35,15 @@ def incremente():
 compteur = 30
 compteur_lbl = tk.Label(canvas, text=str(compteur), font=("", 16))
 compteur_lbl.pack()
+score = 0
 
-
+def changement(str, couleur):
+    """fonction qui permet de changer aléatoirement la couleur et le mot (du label_title)"""
+    global label_title
+    global score
+    label_title["text"]= str
+    label_title["fg"]= couleur
+    score+=1
 
 #Apparition du mot du milieu
 couleurs = ['deep sky blue','firebrick1','spring green2','Gold','DeepPink2','chocolate1','white']
@@ -46,26 +54,21 @@ label_title.pack()
     
 
 label_text = Label(canvas, text="Tapez la couleur des mots et pas le texte des mots !", bg="azure2", font=("Cassia", 20))
-label_score = Label(canvas, text="Score :", bg ="azure2", font=("Cassia", 18))
-
-
-def changement(str):
-    global label_title
-    label_title["text"]= str
-    
+label_score = Label(canvas, text="Score : " + str(score), bg ="azure2", font=("Cassia", 18))
 
 
 
 #ajout des boutons
+
 first_button = Button(canvas, text="Démarrer", command=incremente)
 second_button = Button(canvas, text="réinitialiser", command = réinitialiser)
-third_button = Button(canvas, text='Bleu', bg=couleurs[0], command= lambda : changement(rd.choice(mots)))
-fourth_button = Button(canvas, text='Rouge', bg=couleurs[1], command = lambda: changement(rd.choice(mots)))
-fifth_button = Button(canvas, text='Vert', bg=couleurs[2], command= lambda : changement(rd.choice(mots)))
-sixth_button = Button(canvas, text='Jaune', bg=couleurs[3], command= lambda : changement(rd.choice(mots)))
-seventh_button = Button(canvas, text='Rose', bg=couleurs[4], command= lambda : changement(rd.choice(mots)))
-eighth_button = Button(canvas, text='Orange', bg=couleurs[5], command= lambda : changement(rd.choice(mots)))
-ninth_button = Button(canvas, text='Blanc', bg=couleurs[6], command= lambda : changement(rd.choice(mots)))
+third_button = Button(canvas, text='Bleu', bg=couleurs[0], command= lambda : changement(rd.choice(mots), rd.choice(couleurs)))
+fourth_button = Button(canvas, text='Rouge', bg=couleurs[1], command = lambda: changement(rd.choice(mots), rd.choice(couleurs)))
+fifth_button = Button(canvas, text='Vert', bg=couleurs[2], command= lambda : changement(rd.choice(mots), rd.choice(couleurs)))
+sixth_button = Button(canvas, text='Jaune', bg=couleurs[3], command= lambda : changement(rd.choice(mots), rd.choice(couleurs)))
+seventh_button = Button(canvas, text='Rose', bg=couleurs[4], command= lambda : changement(rd.choice(mots), rd.choice(couleurs)))
+eighth_button = Button(canvas, text='Orange', bg=couleurs[5], command= lambda : changement(rd.choice(mots), rd.choice(couleurs)))
+ninth_button = Button(canvas, text='Blanc', bg=couleurs[6], command= lambda : changement(rd.choice(mots), rd.choice(couleurs)))
 
 
 
